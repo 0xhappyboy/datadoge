e6d9dd5977ba1ad7810cb6cba690d2e1cc70e5c4
0xhappyboy <superhappyboy1995@gmail.com>
2023-10-07 17:13:52 +0800

    init : init

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,15 @@
+
+import { Toolbar } from 'primereact/toolbar';
+
+
+interface PropsType {
+}
+const IndexPage: React.FC<PropsType> = ({ }) => {
+    return (
+        <>
+            <Toolbar />
+        </>
+    );
+};
+
+export default IndexPage;
