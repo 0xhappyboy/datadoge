11e8be5d06a9c6ec107e31074264abb972aaef8d
0xhappyboy <superhappyboy1995@gmail.com>
2023-10-12 12:15:25 +0800

    update : 1. add commonly used contract configurations

---
+++ app/api/demo.js
@@ -0,0 +1,7 @@
+//hello.js 
+function Hello() { 
+    this.func1 = function() { 
+        console.log(123);
+    }; 
+}; 
+module.exports = Hello;
---
+++ app/api/events/contract.js
@@ -0,0 +1,5 @@
+function ContractEvent() {
+  // monitor the latest deployed contract
+  this.listenLastDeploy = function () {};
+}
+module.exports = ContractEvent;
